f603ac02da4063ceb31fd3d753d7b6cac99dae59
sakots <sakots8000@gmail.com>
2025-01-15 18:27:16 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import MyHand from "./components/MyHand"
+
+function App() {
+  return (
+    <>
+      <h1>駅雀 2025</h1>
+      <div className="table">
+        <MyHand />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MyHand.tsx
@@ -0,0 +1,13 @@
+import "./MyHand.css"
+
+const MyHand = () => {
+  return (
+    <>
+      <div className="cards">
+        <button type="submit" className="card"><span className="s">南</span>浦和</button>
+      </div>
+    </>
+  )
+}
+
+export default MyHand
