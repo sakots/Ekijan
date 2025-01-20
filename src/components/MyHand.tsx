16d631e7a81e5872aa14992aec6c5d0acd3bc647
sakots <sakots8000@gmail.com>
2025-01-20 12:45:13 +0900

    手牌

--- src/components/MyHand.tsx
+++ src/components/MyHand.tsx
@@ -1,10 +1,21 @@
+import { useContext } from "react";
+import Wall from "./Wall";
 import "./Tiles.css"
 
 const MyHand = () => {
+  const wall = useContext(Wall);
+  const hand = wall.slice(0,14);
+  const sortedHand = hand.sort((a, b) => a.id - b.id);
+  console.log(sortedHand);
+  const sortedHandList = sortedHand.map((sortedHand) => (
+    <button type="button" key={sortedHand.id} className="tile">
+      {sortedHand.name}
+    </button>
+  ));
   return (
     <>
       <div className="hand">
-        <button type="submit" className="tile">南浦和</button>
+        {sortedHandList}
       </div>
     </>
   )
