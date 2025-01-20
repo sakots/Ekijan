1056dace365c74ce4924472f5a4fd0388e469488
sakots <sakots8000@gmail.com>
2025-01-21 07:35:22 +0900

    fix: ツモ牌ともともとの手牌の分離

--- src/components/MyHand.tsx
+++ src/components/MyHand.tsx
@@ -4,8 +4,13 @@ import "../styles/Tiles.css";
 
 const MyHand = () => {
   const wall = useContext(Wall);
-  const hand = wall.slice(0,14);
+  const hand = wall.slice(0,13);
   const sortedHand = hand.sort((a, b) => a.id - b.id);
+  const draw = wall.slice(13,14);
+  console.log(draw);
+  const drawList = draw.map((draw) => (
+    <button type="button" key={draw.id} className="tile" title={draw.name} dangerouslySetInnerHTML={{ __html: draw.display }}></button>
+  ));
   console.log(sortedHand);
   const sortedHandList = sortedHand.map((sortedHand) => (
     <button type="button" key={sortedHand.id} className="tile" title={sortedHand.name} dangerouslySetInnerHTML={{ __html: sortedHand.display }}></button>
@@ -14,6 +19,7 @@ const MyHand = () => {
     <>
       <div className="hand">
         {sortedHandList}
+        {drawList}
       </div>
     </>
   )
