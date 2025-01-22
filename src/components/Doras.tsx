8df1108c17ee4a09cc0dfeac1f32e842f47c3399
sakots <sakots8000@gmail.com>
2025-01-22 10:29:16 +0900

    裏ドラ

--- src/components/Doras.tsx
+++ src/components/Doras.tsx
@@ -5,14 +5,21 @@ import "../styles/Tiles.css";
 const Doras = () => {
   const wall = useContext(Wall);
   console.log(wall);
-  const dora = wall.slice(-5);
+  const dora = wall.slice(-10, -5);
   const doraList = dora.map((dora) => (
     <button type="button" key={dora.id} className="tile" title={dora.name} dangerouslySetInnerHTML={{ __html: dora.display }}></button>
+  ));
+  const uraDora = wall.slice(-5);
+  const uraDoraList = uraDora.map((uraDora) => (
+    <button type="button" key={uraDora.id} className="tile" title={uraDora.name} dangerouslySetInnerHTML={{ __html: uraDora.display }}></button>
   ));
   return (
     <>
       <div className="dora">
         {doraList}
+      </div>
+      <div className="uraDora">
+        {uraDoraList}
       </div>
     </>
   );
