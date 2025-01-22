0fe6d2309647cb6c07cf0c3f1db1b394eb2fac3b
sakots <sakots8000@gmail.com>
2025-01-22 13:46:32 +0900

    改行

--- src/components/MyHand.tsx
+++ src/components/MyHand.tsx
@@ -6,15 +6,16 @@ const MyHand = () => {
   const wall = useContext(Wall);
   const hand = wall.slice(0,13);
   const sortedHand = hand.sort((a, b) => a.id - b.id);
-  const draw = wall.slice(13,14);
-  console.log(draw);
-  const drawTile = draw.map((draw) => (
+  const firstDraw = wall.slice(13,14);
+  console.log(firstDraw);
+  const drawTile = firstDraw.map((draw) => (
     <button type="button" key={draw.id} className="tile" title={draw.name} dangerouslySetInnerHTML={{ __html: draw.display }}></button>
   ));
   console.log(sortedHand);
   const sortedHandList = sortedHand.map((sortedHand) => (
     <button type="button" key={sortedHand.id} className="tile" title={sortedHand.name} dangerouslySetInnerHTML={{ __html: sortedHand.display }}></button>
   ));
+
   return (
     <>
       <div className="hand">
