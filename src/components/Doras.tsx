2b23000b004c4bd35a24843f6689adff358c1b50
sakots <sakots8000@gmail.com>
2025-01-22 10:35:33 +0900

    ドラ牌の名前隠し

--- src/components/Doras.tsx
+++ src/components/Doras.tsx
@@ -7,11 +7,11 @@ const Doras = () => {
   console.log(wall);
   const dora = wall.slice(-10, -5);
   const doraList = dora.map((dora) => (
-    <button type="button" key={dora.id} className="tile" title={dora.name} dangerouslySetInnerHTML={{ __html: dora.display }}></button>
+    <button type="button" key={dora.id} className="tile" title="ドラ" dangerouslySetInnerHTML={{ __html: dora.display }}></button>
   ));
   const uraDora = wall.slice(-5);
   const uraDoraList = uraDora.map((uraDora) => (
-    <button type="button" key={uraDora.id} className="tile" title={uraDora.name} dangerouslySetInnerHTML={{ __html: uraDora.display }}></button>
+    <button type="button" key={uraDora.id} className="tile" title="裏ドラ" dangerouslySetInnerHTML={{ __html: uraDora.display }}></button>
   ));
   return (
     <>
