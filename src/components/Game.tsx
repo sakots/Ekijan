2a26fcab3722f878969e841c811d23fd16f6ab92
sakots <sakots8000@gmail.com>
2025-02-04 14:01:55 +0900

    なんでやってなる

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import Wall from "./Wall";
 import "../styles/Tiles.css";
 
@@ -6,21 +6,22 @@ const Game = () => {
 
   const [deck, setDeck] = useState(useContext(Wall));
   setDeck(deck.sort(() => Math.random() - Math.random()));
-  const [hand, setHand] = useState([]);
-  const [discardPile, setDiscardPile] = useState([]);
+  const [hand, setHand] = useState<{ name: string; display: string; id: number; sort: number; number: number[]; tags: string[]; wind?: string[]; dora?: string[]; dragon?: string[]; }[]>([]);
+  const [discardPile, setDiscardPile] = useState<{ name: string; display: string; id: number; sort: number; number: number[]; tags: string[]; wind?: string[]; dora?: string[]; dragon?: string[]; }[]>([]);
+  const [pointer, setPointer] = useState(0)
 
   //初期配牌
   const dealFirstHand = () => {
-    const firstHand = [];
-    for (let i = 0; i < 13; i++) {
-      firstHand.push(deck.shift());
-    }
-    return setHand(firstHand);
+    const firstHand = deck.slice(0,13)
+    setPointer(13)
+    return setHand(firstHand as { name: string; display: string; id: number; sort: number; number: number[]; tags: string[]; wind?: string[]; dora?: string[]; dragon?: string[]; }[]);
   };
+
   //ツモ
   const drawTile = () => {
-    const drawn = [];
-    return drawn.push(deck.shift());
+    const drawn = deck.slice(pointer, pointer +1)
+    setPointer(pointer +1)
+    return drawn
   };
   // 牌を捨てる
   const discardTile = (index: number) => {
@@ -40,6 +41,12 @@ const Game = () => {
   const justDrawnTile = drawTile().map((draw) => (
     <button type="button" key={draw.id} className="tile" title={draw.name} dangerouslySetInnerHTML={{ __html: draw.display }}></button>
   ));
+
+  useEffect(() => {
+    dealFirstHand();
+  })
+
+  console.log(hand)
 
   return (
     <>
