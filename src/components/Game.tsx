682f9f99a0b0500b1f9c62a15716d084742f4649
sakots <sakots8000@gmail.com>
2025-02-04 13:22:47 +0900

    うーん

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,20 +3,22 @@ import Wall from "./Wall";
 import "../styles/Tiles.css";
 
 const Game = () => {
+
   const [deck, setDeck] = useState(useContext(Wall));
+  setDeck(deck.sort(() => Math.random() - Math.random()));
   const [hand, setHand] = useState([]);
   const [discardPile, setDiscardPile] = useState([]);
 
   //初期配牌
-  const dealFirstHand = (deck:[]) => {
+  const dealFirstHand = () => {
     const firstHand = [];
     for (let i = 0; i < 13; i++) {
       firstHand.push(deck.shift());
     }
-    return firstHand;
+    return setHand(firstHand);
   };
   //ツモ
-  const drawTile = (deck:[]) => {
+  const drawTile = () => {
     const drawn = [];
     return drawn.push(deck.shift());
   };
@@ -27,8 +29,25 @@ const Game = () => {
     setHand(hand.filter((_, i) => i !== index));
   };
 
+  //手牌をソート
+  const sortedHand = () => {
+    return hand.sort((a, b) => a.id - b.id);
+  }
+  const sortedHandList = sortedHand().map((sortedHand) => (
+    <button type="button" key={sortedHand.id} className="tile" title={sortedHand.name} dangerouslySetInnerHTML={{ __html: sortedHand.display }}></button>
+  ));
+
+  const justDrawnTile = drawTile().map((draw) => (
+    <button type="button" key={draw.id} className="tile" title={draw.name} dangerouslySetInnerHTML={{ __html: draw.display }}></button>
+  ));
+
   return (
-    <></>
+    <>
+      <div className="hand">
+        {sortedHandList}
+        {justDrawnTile}
+      </div>
+    </>
   );
 };
 
